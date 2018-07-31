f3a2c4e9a060cfc6cecb161bb1e259e0105bbe42
Yoonseop Kang <e0engoon@gmail.com>
2018-07-31 12:21:50 +0900

    Initial commit, add cppzmq

---
+++ README.md
@@ -0,0 +1,7 @@
+# homebrew-mine
+
+A tap to manage libraries w/ homebrew.
+
+You can tap this repository by running the following:
+
+    brew tap e0en/mine
---
+++ cppzmq.rb
@@ -0,0 +1,22 @@
+# code taken from https://github.com/bertjwregeer/homebrew-mine
+require 'formula'
+
+class Cppzmq < Formula
+  homepage 'https://github.com/zeromq/cppzmq'
+  url 'https://github.com/zeromq/cppzmq.git'
+  version '0.0.0-brew'
+
+  depends_on 'cmake' => :build
+  depends_on 'zeromq'
+
+  def install
+      system "echo '#{prefix}'"
+      system "cmake ."
+      include.install "zmq.hpp"
+      include.install "zmq_addon.hpp"
+      (lib/'cmake'/'cppzmq').install "cppzmqConfig.cmake"
+      (lib/'cmake'/'cppzmq').install "cppzmqConfigVersion.cmake"
+      (lib/'cmake'/'cppzmq').install "CMakeFiles/Export/share/cmake/cppzmq/cppzmqTargets.cmake"
+      (lib/'cmake'/'cppzmq').install "libzmq-pkg-config"
+  end
+end
