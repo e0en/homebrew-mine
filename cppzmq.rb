bb874f03b93dfe0f08b6d6d831cae23de8f63153
Yoonseop Kang <e0en@users.noreply.github.com>
2020-08-26 11:37:16 +0900

    Update cppzmq version to 4.6.0

--- cppzmq.rb
+++ cppzmq.rb
@@ -4,7 +4,7 @@ require 'formula'
 class Cppzmq < Formula
   homepage 'https://github.com/zeromq/cppzmq'
   url 'https://github.com/zeromq/cppzmq.git'
-  version '4.4.1-brew'
+  version '4.6.0-brew'
 
   depends_on 'cmake' => :build
   depends_on 'zeromq'
